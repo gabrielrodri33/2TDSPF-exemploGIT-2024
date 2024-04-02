c74eabb09275e4f9a4755e821874d59d3ed0d329
gabrielrodri33 <gabrielrodri333@gmail.com>
2024-04-02 11:31:41 -0300

    Reedição do Readme de documentos e classe Calculadora.java

--- Fontes/ProjetoElevador/src/FontesAppElevador/Calculadora.java
+++ Fontes/ProjetoElevador/src/FontesAppElevador/Calculadora.java
@@ -1,6 +1,7 @@
 package FontesAppElevador;
 
 //Classe de entidade - Calculadora aritmética de apoio para estimar a carga do elevador
+// Soma Subtração Multiplicação e Divisão
 public class Calculadora {
 
 	//Atributo de resultado de cálculo
